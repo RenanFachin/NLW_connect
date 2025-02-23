e814aeac309dedcf6826a831254d036578ca05fe
Renan <renan.fachin@hotmail.com>
2025-02-23 13:20:18 -0300

    chore: install tailwindmerge to concatenate stylizations

--- web/src/components/button.tsx
+++ web/src/components/button.tsx
@@ -1,4 +1,5 @@
 import type { ComponentProps, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ComponentProps<"button"> {
 	children: ReactNode;
@@ -8,11 +9,15 @@ interface ButtonProps extends ComponentProps<"button"> {
 export function Button({
 	children,
 	isIconButton = false,
+	className,
 	...props
 }: ButtonProps) {
 	return isIconButton ? (
 		<button
-			className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-500"
+			className={twMerge(
+				"p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-500",
+				className,
+			)}
 			{...props}
 		>
 			{children}
